c0c16ca4a291deb60a748b23c0e96e1d41a7c3aa
KarolinaUser <karolina.krakow@o2.pl>
2023-02-17 19:40:31 +0100

    projekt 2

--- src/main.js
+++ src/main.js
@@ -1,18 +1,57 @@
 /* eslint-disable */  
 
 //PROJEKT 1
-const arrowBtn = document.querySelector('.arrow')
-const arrowIcon = document.querySelector('.fas')
-const img = document.querySelector('.item1')
+// const arrowBtn = document.querySelector('.arrow')
+// const arrowIcon = document.querySelector('.fas')
+// const img = document.querySelector('.item1')
 
-const showImg = () => {
-   img.classList.toggle('hide')
+// const showImg = () => {
+//    img.classList.toggle('hide')
 
-   if(img.classList.contains('hide')) {
-      arrowIcon.style.transform = 'rotate(180deg)'
-   } else {
-      arrowIcon.style.transform = 'rotate(0)'
+//    if(img.classList.contains('hide')) {
+//       arrowIcon.style.transform = 'rotate(180deg)'
+//    } else {
+//       arrowIcon.style.transform = 'rotate(0)'
+//    }
+// } 
+
+// arrowBtn.addEventListener('click', showImg)
+
+//PROJEKT 2
+
+const sizeUp = document.querySelector('.sizeUp')
+const sizeDown = document.querySelector('.sizeDown')
+const colorBtn = document.querySelector('.color')
+const par = document.querySelector('p')
+
+let fontSize = 36
+
+
+const sizeBig = () => {
+ fontSize += 5
+ par.style.fontSize =  fontSize + 'px'
+}
+
+const sizeSmall = () => {
+   if(fontSize <= 21){
+      return
    }
-} 
+   fontSize -= 5
+   par.style.fontSize = fontSize + 'px'
+   
+}
 
-arrowBtn.addEventListener('click', showImg)
+
+const colorChange = () => {
+   const r = Math.floor(Math.random() * 255)
+   const g = Math.floor(Math.random() * 255)
+   const b = Math.floor(Math.random() * 255)
+
+   par.style.color = `rgb(${r}, ${g}, ${b})`
+}
+
+sizeUp.addEventListener('click', sizeBig)
+
+sizeDown.addEventListener('click', sizeSmall)
+
+colorBtn.addEventListener('click', colorChange)
